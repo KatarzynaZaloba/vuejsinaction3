bc3e19a16494d7c5897398c139e12a058b187007
KatarzynaAfrontech <kzalobafrontend@gmail.com>
2026-03-19 20:33:14 +0100

    refactor main.js to use Vue instance instead of createApp

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+require('./assets/app/css')
 
-createApp(App).use(router).mount('#app')
+// createApp(App).use(router).mount('#app')
+
+Vue.config.productionTip = false;
+
+new Vue({
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: { App }
+})
