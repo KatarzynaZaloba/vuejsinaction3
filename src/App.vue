2bec15eecebbd6b0adcae25d0b5476760138834b
Katarzyna Bełdzińska <123471023+KatarzynaZaloba@users.noreply.github.com>
2026-04-07 19:45:30 +0200

    refactor App component by removing unused navigation and cleaning up styles

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
-<!-- <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/form">Form</router-link>
-  </nav>
-  <router-view/>
-</template> -->
 <template>
   <div id="app">
     <transition name="fade" mode="out-in">
-      <router-view />
+      <router-view></router-view>
     </transition>
   </div>
 </template>
@@ -20,34 +13,17 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+#app {}
 
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 0.5s ease-out;
+  transition: opacity .5s ease-out;
+
 }
 
 .fade-enter,
 .fade-leave-to {
   opacity: 0;
+
 }
 </style>
