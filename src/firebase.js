3b86fce0f16bc2e583ec189e544c9277d548af0b
Katarzyna Bełdzińska <123471023+KatarzynaZaloba@users.noreply.github.com>
2026-06-07 12:11:26 +0200

    feat: update Firebase integration and initialize VueFire in main.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,5 +22,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export const db = app.database();
-export const productsRef = db.ref('products');
+export const db = getDatabase(app);
+export const productsRef = ref(db, 'products');
--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue'
 import router from './router'
 import './assets/app.css'
 import {store} from './store/store'
+import firebase from './firebase'
+import './firebase'
+import VueFire from 'vuefire'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
+app.use(VueFire)
 
 store.dispatch('initStore')
 
