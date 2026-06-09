2457efba6a8c71c8f8f9ffbe7e6f54d9660c8c0e
Katarzyna Bełdzińska <123471023+KatarzynaZaloba@users.noreply.github.com>
2026-06-09 20:33:03 +0200

    feat: enhance header component with user authentication buttons

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,11 +9,25 @@
                 </h1>
             </div>
             <div class="nav navbar-nav navbar-right cart">
-                <router-link active-class="active" tag="button" class="btn btn-default btn-lg" :to="{ name: 'Form' }">
-                    <span class="glyphicon glyphicon-shopping-cart">
-                        {{ cartItemCount }}
-                    </span> Kasa
-                </router-link>
+                <div v-if="!mySession">
+                    <button type="button" class="btn btn-default btn-lg" @click="signIn">
+                        Logowanie
+                    </button>
+                </div>
+                <div v-else>
+                    <button type="button" class="btn btn-default btn-lg" @click="signOut">
+                        <img class="photo" :src="mySession.photoURL" alt="profile" /> Wyloguj
+                    </button>
+                </div>
+                <div class="nav navbar-nav navbar-right cart">
+                    <router-link active-class="active" tag="button" class="btn btn-default btn-lg"
+                        :to="{ name: 'Form' }">
+                        <span class="glyphicon glyphicon-shopping-cart">
+                            {{ cartItemCount }}
+                        </span> Kasa
+                    </router-link>
+                </div>
+
             </div>
         </div>
     </header>
@@ -39,6 +53,7 @@ a {
     text-decoration: none;
     color: black;
 }
+
 .router-link-exact-active {
     color: blue;
 }
