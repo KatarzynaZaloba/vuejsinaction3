3855b1d30e3b4850b6838ed2c26bb307454de453
Katarzyna Bełdzińska <123471023+KatarzynaZaloba@users.noreply.github.com>
2026-06-17 09:00:28 +0200

    chore: update dependencies and configure webpack for testing
    - Updated @vue/test-utils to version 2.4.11
    - Updated chromedriver to version 115.0.1
    - Added new dependencies: expect, jsdom, jsdom-global, mocha, webpack, webpack-cli, and webpack-node-externals
    - Enhanced vue.config.js to configure webpack for test environment with externals and source maps
    - Added new file webpack.generated.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
+const nodeExternals = require('webpack-node-externals')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'test') {
+      config.externals = [nodeExternals()]
+      config.devtool = 'inline-cheap-module-source-map'
+    }
+  }
 })
