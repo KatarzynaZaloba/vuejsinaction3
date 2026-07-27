0383ed7945f8467f7fbbd9807d4d57082b46e982
Katarzyna Bełdzińska <123471023+KatarzynaZaloba@users.noreply.github.com>
2026-07-27 20:46:43 +0200

    test: add unit test to verify 'navbar' class is applied to the first div in Header component

--- test/Header.spec.js
+++ test/Header.spec.js
@@ -14,5 +14,13 @@ describe('Header.vue', () => {
             store, localVue, propsData: {cartItemCount}
         })
         expect(wrapper.vm.cartItemCount).toBe(cartItemCount);
+    }),
+    it('Sprawdzam, czy do pierwszego elementu div została dodana klasa navbar', () => {
+        const cartItemCount = 10;
+        const wrapper = shallowMount(Header, {
+            store, localVue, propsData: {cartItemCount}
+        })
+        const p = wrapper.findAll('div').at(0);
+        expect(p.classes()).toContain('navbar');
     })
 })
