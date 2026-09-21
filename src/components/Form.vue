05a82d48c8afb9c052a1e123a85037cbb46dc587
Katarzyna Bełdzińska <123471023+KatarzynaZaloba@users.noreply.github.com>
2026-09-21 20:57:14 +0200

    feat: translate form labels and messages from Polish to English for better accessibility

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,43 +1,51 @@
 <template>
   <div>
     <my-header :cartItemCount="cartItemCount"></my-header>
-    <div class="row">
-      <div class="col-md-10 col-md-offset-1">
-        <div class="panel panel-info">
-          <div class="panel-heading">Formularz zamówienia</div>
-          <div class="panel-body">
+    <div class="">
+      <div class=""><button class=""><svg class="" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
+          </svg>Back to shop</button>
+        <h1 class="">Your <span class="">cart</span></h1>
+        <p class="">Your cart is empty</p>
+      </div>
+    </div>
+    <div class="">
+      <div class="">
+        <div class="">
+          <div class="">Delivery details</div>
+          <div class="">
             <div class="form-group">
-              <div class="col-md-12">
-                <h4><strong>Podaj informacje o sobie</strong></h4>
+              <div class="">
+                <h4><strong>Tell us where to send your order</strong></h4>
               </div>
             </div>
             <div class="form-group">
-              <div class="col-md-6">
-                <strong>Imię:</strong>
+              <div class="">
+                <strong>First name:</strong>
                 <input v-model.trim="order.firstName" class="form-control" />
               </div>
-              <div class="col-md-6">
-                <strong>Nazwisko:</strong>
+              <div class="">
+                <strong>Last name:</strong>
                 <input v-model.trim="order.lastName" class="form-control" />
               </div>
             </div>
             <div class="form-group">
-              <div class="col-md-12"><strong>Adres:</strong></div>
-              <div class="col-md-12">
+              <div class=""><strong>Address:</strong></div>
+              <div class="">
                 <input v-model.trim="order.address" class="form-control" />
               </div>
             </div>
             <div class="form-group">
-              <div class="col-md-12"><strong>Miejscowość:</strong></div>
-              <div class="col-md-12">
+              <div class=""><strong>City:</strong></div>
+              <div class="">
                 <input v-model.trim="order.city" class="form-control" />
               </div>
             </div>
             <div class="form-group">
-              <div class="col-md-2">
-                <strong>Województwo:</strong>
+              <div class="">
+                <strong>State:</strong>
                 <select v-model="order.state" class="form-control">
-                  <option disabled value="">Województwo</option>
+                  <option disabled value="">State</option>
                   <option v-for="(state, key) in states" v-bind:value="state">
                     {{ key }}
                   </option>
@@ -45,41 +53,41 @@
               </div>
             </div>
             <div class="form-group">
-              <div class="col-md-6 col-md-offset-4">
-                <strong>Kod pocztowy:</strong>
+              <div class="">
+                <strong>Zip code:</strong>
                 <input v-model.number="order.zip" class="form-control" type="number" />
               </div>
             </div>
             <div class="form-group">
-              <div class="col-md-6 boxes">
+              <div class="">
                 <input type="checkbox" id="gift" value="true" v-bind:true-value="order.sendGift"
                   v-bind:false-value="order.dontSendGift" v-model="order.gift">
-                <label for="gift">Wysłać jako prezent?</label>
+                <label for="gift">Send as a gift?</label>
               </div>
             </div><!-- koniec form-group -->
             <div class="form-group">
-              <div class="col-md-6 boxes">
+              <div class="">
                 <input type="radio" id="home" v-bind:value="order.home" v-model="order.method">
-                <label for="home">Domowy</label>
+                <label for="home">Home</label>
                 <input type="radio" id="business" v-bind:value="order.business" v-model="order.method">
-                <label for="business">Firmowy</label>
+                <label for="business">Business</label>
               </div>
             </div><!-- koniec form-group-->
             <div class="form-group">
-              <div class="col-md-6">
-                <button type="submit" class="btn btn-primary submit" v-on:click="submitForm">Złóż zamówienie</button>
+              <div class="">
+                <button type="submit" class="btn btn-primary submit" v-on:click="submitForm">Place order</button>
               </div><!-- koniec col-md-6-->
             </div><!-- koniec form-group-->
-            <div class="col-md-12 verify">
+            <div class="">
               <pre>
-                         Imię: {{ order.firstName }}
-                     Nazwisko: {{ order.lastName }}
-                        Adres: {{ order.address }}
-                  Miejscowość: {{ order.city }}
-                 Kod pocztowy: {{ order.zip }}
-                  Województwo: {{ order.state }}
-                       Metoda: {{ order.method }}
-                      Prezent: {{ order.gift }}
+                         First name: {{ order.firstName }}
+                     Last name: {{ order.lastName }}
+                        Address: {{ order.address }}
+                  City: {{ order.city }}
+                 Zip code: {{ order.zip }}
+                  State: {{ order.state }}
+                       Method: {{ order.method }}
+                      Gift: {{ order.gift }}
               </pre>
             </div><!-- koniec col-md-12 verify-->
           </div><!-- koniec panel-body-->
